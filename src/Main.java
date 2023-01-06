325ac0ec45f57bd6c77801d9473a2cdc4c7557cc
Jonathan <jonathan.ribeiro28121997@gmail.com>
2023-01-05 22:55:42 -0300

    Class Filme and Trailler.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import br.com.dio.desafio.dominio.Filme;
+import br.com.dio.desafio.dominio.Trailler;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Filme filme1 = new Filme();
+
+        filme1.setTitulo("Avatar");
+        filme1.setSinopse("Ação e aventura aclamada pelo publico, onde um paralitica viaja para um mundo no corpo de um avatar, possibilitando coisas empolgantes e grandes responsabilidades.");
+        filme1.setCargaHoraria(150);
+        System.out.println(filme1);
+
+        Trailler mentoria = new Trailler();
+
+        mentoria.setTitulo("Trailler de Avatar!");
+        mentoria.setDescricao("Ficção científica/Ação");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Filme.java
@@ -0,0 +1,43 @@
+package br.com.dio.desafio.dominio;
+
+public class Filme {
+    private String titulo;
+    private String sinopse;
+     private int cargaHoraria;
+
+    public Filme() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + sinopse + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
